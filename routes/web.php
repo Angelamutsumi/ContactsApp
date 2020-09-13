5df19d357f448cc4c446f66443ca9e35dac15d84
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-09-13 11:18:52 -0300

    Cria função create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', [ContactController::class, 'index']);
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contact_register');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contact_store');
